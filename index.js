25de1c4d06e4cec2ea83632166f3b5d6a6f94e7e
Jesus jose martha laborin <polibos6366@gmail.com>
2025-01-15 11:29:57 -0700

    routing in express'

--- index.js
+++ index.js
@@ -3,7 +3,20 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.sendFile("./static/index.html", { root: __dirname });
+  res.send("Hello world");
+});
+
+app.get("/about", (req, res) => {
+  res.send("About");
+});
+
+app.get("/weather", (req, res) => {
+  res.send("The current weather is nice");
+});
+
+// midleware
+app.use((req, res) => {
+  res.status(404).send("Page not found");
 });
 
 app.listen(3000, () => {
