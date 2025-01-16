bf4326cf026ed8c0ccfe1da429bfb31207d05e67
Jesus jose martha laborin <polibos6366@gmail.com>
2025-01-16 09:11:20 -0700

    middleware interceptor

--- index.js
+++ index.js
@@ -1,39 +1,30 @@
 const express = require("express");
+const morgan = require("morgan");
 
 const app = express();
 
-app.get("/search", (req, res) => {
-  if (req.query.q === "javascript books") {
-    return res.send("List of books about javascript");
+// midlewares
+app.use(morgan("dev"));
+
+app.use((req, res, next) => {
+  console.log("paso por aqui");
+  next();
+});
+
+app.get("/profile", (req, res) => {
+  res.send("profile page");
+});
+
+app.use((req, res, next) => {
+  if (req.query.login === "jesus") {
+    next();
+  } else {
+    res.send("Not authorized");
   }
-
-  res.send("Normal page");
 });
 
-app.get("/hello/:user", (req, res) => {
-  console.log(req.query);
-  res.send(req.query);
-});
-
-app.get("/add/:x/:y", (req, res) => {
-  console.log(req.params.x);
-  console.log(req.params.y);
-
-  const result = Number(req.params.x) + Number(req.params.y);
-
-  console.log(result);
-
-  res.send(`Result: ${result}`);
-});
-
-app.get("/users/:username/photo", (req, res) => {
-  if (req.params.username === "jesus") {
-    return res.sendFile("./javascript.png", {
-      root: __dirname,
-    });
-  }
-
-  res.send("User not authorized");
+app.get("/dashboard", (req, res) => {
+  res.send("Dashboard Page");
 });
 
 app.listen(3000, () => {
